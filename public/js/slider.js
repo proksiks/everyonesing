42ffdaecd52301a2b20479ce8323e8119cde34bf
Khliupnev Vladimir <proksik49032@mail.ru>
2025-12-01 00:50:36 +0400

    feat: add slider

--- public/js/slider.js
+++ public/js/slider.js
@@ -1,22 +1,42 @@
 import Swiper from "https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.mjs";
-
-const maoinSlider = new Swiper(".main-slider", {
+const textSlider = new Swiper(".main-slider-text", {
+  loop: true,
+  effect: "fade",
+  fadeEffect: {
+    crossFade: true,
+  },
+  watchSlidesProgress: true,
+  allowTouchMove: false,
+});
+new Swiper(".main-slider", {
   effect: "coverflow",
   grabCursor: true,
   loop: true,
   centeredSlides: true,
   slidesPerView: "auto",
-  centeredSlides: true,
-  coverflowEffect: {
-    rotate: 80,
-    stretch: 150,
-    depth: 700,
-    modifier: 1,
-    slideShadows: true,
-  },
   navigation: {
     nextEl: ".main-slider__button--next",
     prevEl: ".main-slider__button--prev",
   },
+  thumbs: {
+    swiper: textSlider,
+  },
+  breakpoints: {
+    320: {
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 250,
+        depth: 200,
+        modifier: 1,
+        slideShadows: true,
+      },
+    },
+    768: {
+      coverflowEffect: {
+        rotate: 80,
+        stretch: 150,
+        depth: 700,
+      },
+    },
+  },
 });
-maoinSlider.slideTo(2);
